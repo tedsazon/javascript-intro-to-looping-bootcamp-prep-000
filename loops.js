760d96c70bcacefb75a007404159500078ed787e
Ted Sazon <tedsazon@gmail.com>
2018-11-16 17:29:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,4 @@
-var forLoop = []
-
-for (let i = 1; i < 25; i++) {
+var forLoop = for (let i = 1; i < 25; i++) {
   console.log("I am a strange loop.")
   else console.log("I am ${i} strange loops.")
 
