e9b16bd924cb8d01d5460dd25243073da3b38b67
Ted Sazon <tedsazon@gmail.com>
2018-11-16 18:21:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,13 +18,13 @@ function whileLoop(n) {
   return 'done'
 }
 
+
 function doWhileLoop(array) {
   function maybeTrue() {
-    return Math.random() >=0.5
+    return Math.random() >= 0.5
   }
-}
-do {
-  array.pop()
-} while (array.length . -1 && maybeTrue())
-  return array
+   do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
 }
