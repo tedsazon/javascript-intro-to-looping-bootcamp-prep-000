a1315b8aa4896af32a57de42d31c0c91bd723c68
Ted Sazon <tedsazon@gmail.com>
2018-11-16 17:40:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,6 @@
-var forLoop(array) {
-  adds `I am ${i} strange loop{i === 0 ? '' ; 's'}."`
-}
-
-for (let i = 1; i < 25; i++) {
-  console.log("I am a strange loop.")
-  else console.log("I am ${i} strange loops.")
-
-
+function forLoop {
+  for (let i = 1; i < 25; i++) {
+    adds `"I am ${i} strange loop"`
+    else `"I am ${i} strange loops"`
+  }
 }
